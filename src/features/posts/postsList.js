6329cdc297f03c5a5e41f4aa195e0a0c884fbbc5
Francis Githae <mureithigithae@gmail.com>
2022-06-20 09:45:13 +0300

    add edit post functionality

--- src/features/posts/postsList.js
+++ src/features/posts/postsList.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 import { useSelector } from "react-redux";
+import {Link} from 'react-router-dom'
+
 
 export const PostsList= ()=>{
 
@@ -11,6 +13,8 @@ export const PostsList= ()=>{
         <article className="post-excerpt" key={post.id}>
             <h3>{post.title}</h3>
             <p className="post-content">{post.content.substring(0,100)}</p>
+
+            <Link to={`/posts/${post.id}`} className="button muted-button">View Post</Link>
         </article>
     ))
 
